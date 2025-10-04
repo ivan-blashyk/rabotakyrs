74cd1a743db426336d56b70e96c3f30ecbd18058
blashyk_Ivan <blashyk.ivan@gmail.com>
2025-10-04 15:19:01 +0300

    промежуточный результат

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import category,Product
+
+
+def product_list(request,category_slug=None):
+    categories = category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    category = None
+    if category_slug:
+        category = get_object_or_404(category, category_slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request,'main/product/list.html',
+                  {'category' : category,
+                  'categories' : categories,
+                  'products': products})
+
 
 # Create your views here.
